b12fc50763ef358d78682cf453ce2eb29df0be73
Gauthier Bach <gabach@z2r10p7.42lyon.fr>
2026-03-09 15:13:04 +0100

    changed the init of the utiles

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,8 +2,8 @@ from .mlx_display import (
     img_put_px,
     render,
     draw_borders,
-    get_color_palette,
-    get_rainbow_palette
+    get_rainbow_palette,
+    put_str_to_img
 )
 from .events import handle_buttons, global_update
 from .cleanup import clear_all, clear_img
@@ -11,7 +11,7 @@ from .checks import is_in
 __all__ = [
     "img_put_px",
     "draw_borders",
-    "get_color_palette",
+    "put_str_to_img",
     "get_rainbow_palette",
     "handle_buttons",
     "global_update",
